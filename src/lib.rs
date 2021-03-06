0e5f6f7cb060fb30de4cf64df0eb576ddc2390fa
Yuta Hinokuma <yuta.hinokuma725@gmail.com>
2021-03-06 15:02:50 +0900

    3.1. Variables and Methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub mod vector3;
+
 pub fn to_pixel_value(c: f64) -> u8 {
     (c * 255.999) as u8
 }
---
+++ src/vector3.rs
@@ -0,0 +1,151 @@
+use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
+
+#[derive(Debug, Clone, PartialEq)]
+struct Vector3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vector3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Vector3 { x, y, z }
+    }
+    pub fn x(&self) -> f64 {
+        self.x
+    }
+    pub fn y(&self) -> f64 {
+        self.y
+    }
+    pub fn z(&self) -> f64 {
+        self.z
+    }
+}
+
+impl Add for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        &self + &rhs
+    }
+}
+
+impl Add for &Vector3 {
+    type Output = Vector3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::Output::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+impl Sub for Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        &self - &rhs
+    }
+}
+
+impl Sub for &Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::Output::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
+    }
+}
+
+impl AddAssign for Vector3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+impl SubAssign for Vector3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self.z -= rhs.z;
+    }
+}
+
+impl MulAssign<f64> for Vector3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl From<Color> for Vector3 {
+    fn from(c: Color) -> Self {
+        c.0
+    }
+}
+
+impl From<Point3> for Vector3 {
+    fn from(p: Point3) -> Self {
+        p.0
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Point3(Vector3);
+
+impl From<Vector3> for Point3 {
+    fn from(v: Vector3) -> Self {
+        Self(v)
+    }
+}
+
+impl Point3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self(Vector3 { x, y, z })
+    }
+    pub fn x(&self) -> f64 {
+        self.0.x
+    }
+    pub fn y(&self) -> f64 {
+        self.0.y
+    }
+    pub fn z(&self) -> f64 {
+        self.0.z
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Color(Vector3);
+
+impl From<Vector3> for Color {
+    fn from(v: Vector3) -> Self {
+        Self(v)
+    }
+}
+
+impl Color {
+    pub fn new(r: f64, g: f64, b: f64) -> Self {
+        Self(Vector3 { x: r, y: g, z: b })
+    }
+    pub fn r(&self) -> f64 {
+        self.0.x
+    }
+    pub fn g(&self) -> f64 {
+        self.0.y
+    }
+    pub fn b(&self) -> f64 {
+        self.0.z
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_vec() {
+        let v1 = Vector3::new(1.0, 0.0, 0.0);
+        let v2 = Vector3::new(0.0, 1.0, 0.0);
+
+        assert_eq!(v1 + v2, Vector3::new(1.0, 1.0, 0.0))
+    }
+}
